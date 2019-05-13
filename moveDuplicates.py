c26e849d1ecd936a45ffd279ddbf20fe6255c8df
Nischala Raja Shekar <37286118+NischalaRajaShekar@users.noreply.github.com>
2019-05-13 12:36:47 -0700

    Add files via upload

---
+++ moveDuplicates.py
@@ -0,0 +1,15 @@
+#Move duplicates to the end of list
+
+def moveDuplicates(arr):
+    arr.sort()
+    num_list = []
+    dup_list = []
+    for num in arr:
+        if num not in num_list:
+            num_list.append(num)
+        else:
+            dup_list.append(num)
+    return num_list+dup_list
+
+arr = [3,6,2,8,4,6,2,2,1,3]
+print (moveDuplicates(arr))
